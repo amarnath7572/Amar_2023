e6cb872b6dacd033ecf6c4d57466e2f84cdc6b14
amarnath.gangigunta <amarnath.gangigunta@gmail.com>
2023-08-03 13:09:41 +0530

    Aug 3 1 09 PM

---
+++ 1_Scaler/1_Core_Curriculum/5_LLD/Code/create-a-basic-class--student--base/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    // write the code of student class here
+	
+	private int age;
+
+	private String name;
+
+
+	void display(){
+
+	System.out.println("My name is "+this.name+". I am "+this.age+" years old");
+
+	}
+
+	void sayHello(String name){
+
+	System.out.println(this.name+" says hello to "+name);
+
+	}
+}
